48ad8d68cd35c9cad437671fedd0c4343a001b83
teojincheng <jinchengproject@gmail.com>
2020-04-20 15:48:07 +0800

    [jc] update recipe.model to inlcude new properties

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -1,23 +1,30 @@
 const mongoose = require("mongoose");
 
-const recipeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const recipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    cookingTime: {
+      type: String,
+    },
+    ingredients: {
+      type: String,
+    },
+    cookingMethod: {
+      type: String,
+    },
+    savedCount: {
+      type: Number,
+      default: 0,
+    },
   },
-  description: {
-    type: String,
-  },
-  cookingTime: {
-    type: String,
-  },
-  ingredients: {
-    type: String,
-  },
-  cookingMethod: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
